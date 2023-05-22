d9ae0de76c1efc54293c8007aef97d6f54e82852
danishjan <engdanishjan619@gmail.com>
2023-05-22 23:18:02 +0500

    navchanges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import {
   FaTimes,
   FaGithub,
   FaLinkedin,
-  FaFacebook,
   FaLinkedinIn,
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
@@ -17,13 +16,13 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className={`fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300`}>
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '200px' }} />
       </div>
 
-      {/* menu */}
-      <ul className='hidden md:flex'>
+      
+      <ul className={`hidden md:flex`}>
         <li>
           <Link to='home' smooth={true} duration={500}>
             Home
@@ -96,7 +95,7 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className='md:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
@@ -109,7 +108,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://github.com/danishjan'
+              href='/'
             >
               Github <FaGithub size={30} />
             </a>
@@ -117,7 +116,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://mail.google.com/mail/u/0/#inbox'
+              href='www.danishjan8582@gmail.com '
             >
               Email <HiOutlineMail size={30} />
             </a>
