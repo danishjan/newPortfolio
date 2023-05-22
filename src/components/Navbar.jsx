b2d5a8b0db35921a68833a801d8222a8fabf5aef
danishjan <engdanishjan619@gmail.com>
2023-05-22 23:13:09 +0500

    newchages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,13 +17,13 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className={`fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300`}>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '200px' }} />
       </div>
 
-      
-      <ul className={`hidden md:flex`}>
+      {/* menu */}
+      <ul className='hidden md:flex'>
         <li>
           <Link to='home' smooth={true} duration={500}>
             Home
@@ -96,7 +96,7 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className='md:flex fixed flex-col top-[35%] left-0'>
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
@@ -109,7 +109,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/danishjan'
             >
               Github <FaGithub size={30} />
             </a>
@@ -117,7 +117,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='www.danishjan8582@gmail.com '
+              href='https://mail.google.com/mail/u/0/#inbox'
             >
               Email <HiOutlineMail size={30} />
             </a>
