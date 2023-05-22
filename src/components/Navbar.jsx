73eb6726cbdbbcf2ae62233601b2c16181db4cde
danishjan <engdanishjan619@gmail.com>
2023-05-22 20:26:35 +0500

    updated two

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className={nav  ? 'hidden' : 'fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'}>
+    <div className={`fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300`}>
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '200px' }} />
       </div>
